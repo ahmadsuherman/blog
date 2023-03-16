1e9ddeb5f152953b8aaf544e30027171a3be2427
ahmadsuherman <ahmad.223040066@mail.unpas.ac.id>
2023-03-16 13:35:59 +0700

    Video 3 - Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "name"      => "Ahmad Suherman",
+        "email"     => "ahmad.223040066@mail.unpas.ac.id",
+        "image"     =>  "ahmad.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
